6ef40d924cdda63ae89ffe21b19357f62f70019e
김호진 <gimhojin@amsesm.cnm7103.co.kr>
2021-01-11 21:16:57 +0900

    codeinterview

--- codeinterview.py
+++ codeinterview.py
@@ -1,6 +1,7 @@
 palin = "A man, a plan, a canal:Panama"
 notpalin = "race a car"
 
+# 리스트를 이용하는 방법
 def is_palindrome( pa:str ) -> bool:
     newpa = []
     # 영소문자, 숫자만 남긴다
@@ -12,4 +13,16 @@ def is_palindrome( pa:str ) -> bool:
         if newpa.pop(0) != newpa.pop():
             return False
     return True
-    
+
+# 데크 자료형을 이용한 최적화
+def is_palindrome_bydeque(pa:str) -> bool:
+    # 자료형 데크로 선언
+    strs: Deque = collections.deque()
+    for char in pa:
+        if char.isalnum():
+            strs.append(char)
+    # 팰린드롬 여부 판별
+    while(len(pa) > 1):
+        if pa.popleft() != pa.pop():
+            return False
+    return True
