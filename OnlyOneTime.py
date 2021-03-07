0a94f91c0f084bf7caddc992ac2985464626454f
김호진 <gimhojin@gimhojin-ui-MacBookAir.local>
2021-03-07 18:22:08 +0900

    3/7 sun

--- OnlyOneTime.py
+++ OnlyOneTime.py
@@ -1,2 +1,22 @@
-s = [1,2,3,2,1]
-print(s.sort())
+# import random
+
+# TRIAL = 100000
+# same = 0
+# for _ in range(TRIAL):
+#     birthdays = []
+#     for i in range(23):
+#         birthday = random.randint(1,365)
+#         if birthday in birthdays:
+#             same += 1
+#             break
+#         birthdays.append(birthday)
+
+# print(f"{(same/TRIAL * 100)} %")
+
+stones = "aaWSDfadswef"
+jewels = "ws"
+print(sum(map(stones.count, jewels)))
+
+def numJewelsInStones(self, jewels: str, stones: str) -> int:
+    return sum([s in jewels for s in stones])
+        
